b2a1b0467784f7fc6ce879b36e544aa94201ee82
oreste-abizera <oresteabizera11@gmail.com>
2024-04-30 15:58:51 +0200

    feat: create a blob object

--- app/main.js
+++ app/main.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const path = require("path");
+const { stdout } = require("process");
 const zlib = require("zlib");
+const crypto = require("crypto");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 // console.log("Logs from your program will appear here!");
@@ -13,6 +15,9 @@ switch (command) {
     break;
   case "cat-file":
     catFile();
+    break;
+  case "hash-object":
+    hashObject();
     break;
   default:
     throw new Error(`Unknown command ${command}`);
@@ -46,4 +51,29 @@ function catFile() {
   const contentStart = decompressed.indexOf("\x00") + 1;
   const content = decompressed.toString("utf-8", contentStart);
   process.stdout.write(content);
+}
+
+function hashObject() {
+  const command = process.argv[3];
+  const file = process.argv[4];
+  if (!command || !file) {
+    throw new Error("Missing arguments");
+  }
+  if (command !== "-w") {
+    throw new Error(`Unknown flag ${command}`);
+  }
+
+  const hash = hashFile(file);
+  process.stdout.write(hash);
+}
+
+function hashFile(file) {
+  const content = fs.readFileSync(file);
+  const header = `blob ${content.length}\x00`;
+  const store = zlib.deflateSync(Buffer.from(header + content));
+  const hash = crypto.createHash("sha1").update(store).digest("hex");
+  const hashPath = path.join(__dirname, ".git", "objects", hash.slice(0, 2));
+  fs.mkdirSync(hashPath, { recursive: true });
+  fs.writeFileSync(path.join(hashPath, hash.slice(2)), store);
+  return hash;
 }
